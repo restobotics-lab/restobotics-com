df639d84d027ac1d14a02c2a5333bd9180cda097
Exoalam <nafiul.arpon@gmail.com>
2024-02-12 16:55:11 +0600

    Started Implementing last section for the ai rentail store

--- src/components/AboutUs/ServicesAbout.js
+++ src/components/AboutUs/ServicesAbout.js
@@ -14,82 +14,76 @@ function ServicesAbout() {
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-lg-4 col-md-6">
+                        <div className="col-lg-3 col-md-6">
                             <div
                                 className="appie-single-service-2 appie-single-service-about mt-30 wow animated fadeInUp"
                                 data-wow-duration="2000ms"
                                 data-wow-delay="200ms"
                             >
                                 <div className="icon">
-                                    <i className="fal fa-tv"></i>
+                                    <i className="fal fa-building"></i>
                                 </div>
-                                <h4 className="title">Carefully designed</h4>
-                                <p>He lost his bottle loo don't get shirty with me ruddy.</p>
+                                <h4 className="title">Building</h4>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-md-6">
+                        <div className="col-lg-3 col-md-6">
                             <div
                                 className="appie-single-service-2 appie-single-service-about item-2 mt-30 wow animated fadeInUp"
                                 data-wow-duration="2000ms"
                                 data-wow-delay="400ms"
                             >
                                 <div className="icon">
-                                    <i className="fal fa-code"></i>
+                                    <i className="fal fa-gas-pump"></i>
                                 </div>
-                                <h4 className="title">Clean Modern Code</h4>
-                                <p>He lost his bottle loo don't get shirty with me ruddy.</p>
+                                <h4 className="title">Gas Station</h4>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-md-6">
+                        <div className="col-lg-3 col-md-6">
                             <div
                                 className="appie-single-service-2 appie-single-service-about item-3 mt-30 wow animated fadeInUp"
                                 data-wow-duration="2000ms"
                                 data-wow-delay="600ms"
                             >
                                 <div className="icon">
-                                    <i className="fal fa-user-friends"></i>
+                                    <i className="fal fa-hospital"></i>
                                 </div>
-                                <h4 className="title">User Interactive</h4>
-                                <p>He lost his bottle loo don't get shirty with me ruddy.</p>
+                                <h4 className="title">Hospital</h4>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-md-6">
+                        <div className="col-lg-3 col-md-6">
                             <div
                                 className="appie-single-service-2 appie-single-service-about item-4 mt-30 wow animated fadeInUp"
                                 data-wow-duration="2000ms"
                                 data-wow-delay="200ms"
                             >
                                 <div className="icon">
-                                    <i className="fal fa-mobile"></i>
+                                    <i className="fal fa-school"></i>
                                 </div>
-                                <h4 className="title">Choose a App</h4>
-                                <p>He lost his bottle loo don't get shirty with me ruddy.</p>
+                                <h4 className="title">School</h4>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-md-6">
+                        <div className="col-lg-3 col-md-6">
                             <div
                                 className="appie-single-service-2 appie-single-service-about item-5 mt-30 wow animated fadeInUp"
                                 data-wow-duration="2000ms"
                                 data-wow-delay="400ms"
                             >
                                 <div className="icon">
-                                    <i className="fal fa-retweet"></i>
+                                    <i className="fal fa-shopping-bag"></i>
                                 </div>
-                                <h4 className="title">Seamless Sync</h4>
-                                <p>He lost his bottle loo don't get shirty with me ruddy.</p>
+                                <h4 className="title">Shopping Mall</h4>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-md-6">
+                        <div className="col-lg-3 col-md-6">
                             <div
                                 className="appie-single-service-2 appie-single-service-about item-6 mt-30 wow animated fadeInUp"
                                 data-wow-duration="2000ms"
                                 data-wow-delay="600ms"
                             >
                                 <div className="icon">
-                                    <i className="fal fa-bell"></i>
+                                    <i className="fal fa-plane"></i>
                                 </div>
-                                <h4 className="title">Notifications Settings</h4>
-                                <p>He lost his bottle loo don't get shirty with me ruddy.</p>
+                                <h4 className="title">Airport</h4>
                             </div>
                         </div>
                     </div>
